ec00372f2c08f2db08b582a7066cb731ea860b0c
MaheshSaini95 <maheshsaini75318@gmail.com>
2026-01-16 07:57:04 +0530

    Production ready - updated env and CORS

--- config/commissionConfig.js
+++ config/commissionConfig.js
@@ -16,11 +16,11 @@ module.exports = {
     3: { rate: 0.06, amount: 15 }, // 6% or ₹15
     4: { rate: 0.04, amount: 10 }, // 4% or ₹10
     5: { rate: 0.02, amount: 5 }, // 2% or ₹5
-    6: { rate: 0.004, amount: 1 }, // 2% or ₹5
-    7: { rate: 0.004, amount: 1 }, // 2% or ₹5
-    8: { rate: 0.004, amount: 1 }, // 2% or ₹5
-    9: { rate: 0.004, amount: 1 }, // 2% or ₹5
-    10: { rate: 0.004, amount: 1 }, // 2% or ₹5
+    6: { rate: 0.004, amount: 3 }, // 2% or ₹5
+    7: { rate: 0.004, amount: 3 }, // 2% or ₹5
+    8: { rate: 0.004, amount: 2 }, // 2% or ₹5
+    9: { rate: 0.004, amount: 2 }, // 2% or ₹5
+    10: { rate: 0.004, amount: 2 }, // 2% or ₹5
   },
   MAX_COMMISSION_LEVEL: 10,
 
@@ -32,7 +32,7 @@ module.exports = {
     LEVEL_4: [{ teams: 750, reward: 3000, title: "Smart watch - ₹3000" }],
     LEVEL_5: [{ teams: 3750, reward: 15000, title: "Boat speaker - ₹15000" }],
     LEVEL_6: [{ teams: 18750, reward: 75000, title: "Smart mobile - ₹75000" }],
-    LEVEL_7: [{ teams: 93150, reward: 375000, title: "manali tour - ₹375000" }],
+    LEVEL_7: [{ teams: 93750, reward: 375000, title: "manali tour - ₹375000" }],
     LEVEL_8: [{ teams: 468750, reward: 1875000, title: "goa tour - ₹1875000" }],
     LEVEL_9: [
       { teams: 2343750, reward: 9375000, title: "Thailand tour - ₹9375000" },
